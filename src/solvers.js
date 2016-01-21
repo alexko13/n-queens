48e12296e3c5bec81b25493eb8bb4c7908c5fa9f
Alex Ko <alexko@utexas.edu>
2016-01-21 13:06:36 -0700

    implemented countNRooksSolutions

--- src/solvers.js
+++ src/solvers.js
@@ -4,7 +4,7 @@
   \__ \ (_) | |\ V /  __/ |  \__ \
   |___/\___/|_| \_/ \___|_|  |___/
 
-*/
+  */
 
 // hint: you'll need to do a full-search of all possible arrangements of pieces!
 // (There are also optimizations that will allow you to skip a lot of the dead search space)
@@ -20,7 +20,6 @@ window.findNRooksSolution = function(n) {
   while (i < n){
     var row = Math.floor(Math.random()*n);
     var col = Math.floor(Math.random()*n);
-    console.log("row: "+row + "  col: " + col);
     if (board.get(row)[col] !== 1){
       board.togglePiece(row,col);
       if (board.hasAnyRooksConflicts()){
@@ -39,8 +38,24 @@ window.findNRooksSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0; //fixme
+  var board = new Board({"n":n});
 
+  /*var recurse = function(row){
+    if (row === n){
+      solutionCount++;
+      return;
+    }
+    for (var i = 0; i < n; i++){
+      board.togglePiece(row, i);
+      if (!board.hasColConflictAt(i)){
+        recurse(row+1);
+      }
+      board.togglePiece(row, i);
+    }
+  };*/
+
+  recurse(0);
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
