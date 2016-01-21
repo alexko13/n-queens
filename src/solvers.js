2e08435f3a41d5bcfd0c51ea816b12f5475783a8
Alex Ko <alexko@utexas.edu>
2016-01-21 13:52:12 -0700

    implemented countNQueensSolutions some progress on findNQueensSolution

--- src/solvers.js
+++ src/solvers.js
@@ -53,9 +53,9 @@ window.countNRooksSolutions = function(n) {
       }
       board.togglePiece(row, i);
     }
-  };*/
+  };
 
-  recurse(0);
+  recurse(0);*/
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
@@ -65,7 +65,43 @@ window.countNRooksSolutions = function(n) {
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
   var solution = undefined; //fixme
+  var solutionCount = 0;
+  var board = new Board({'n': n});
+  var temp = undefined;
 
+  // for (var i = 0; i < n; i++){
+  //   for (var j = 0; j < n; j++){
+  //     board.togglePiece(i, j);
+  //     if(board.hasAnyQueensConflicts()){
+  //       board.togglePiece(i, j);
+  //     }
+  //   }
+  // }
+
+  var recurse = function(row){
+    if (solution === undefined){
+      if (row === n){
+        solutionCount++;
+        console.log(solution);
+        solution = board.rows().slice();
+        console.log(solution);
+        //temp = JSON.stringify(board.rows());
+        console.log('--------------------------------------------');
+        console.log(JSON.stringify(board.rows()));
+        return;
+      }
+      for (var i = 0; i < n; i++){
+        board.togglePiece(row, i);
+
+        if(!board.hasAnyQueensConflicts()){
+          recurse(row+1);
+        }
+        board.togglePiece(row, i);
+      }
+    }
+  };
+  //solution = JSON.parse(temp);
+  recurse(0);
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
 };
@@ -73,7 +109,25 @@ window.findNQueensSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0; //fixme
+  var board = new Board({'n': n});
+
+  /*var recurse = function(row){
+    if (row === n){
+      solutionCount++;
+      return;
+    }
+    for (var i = 0; i < n; i++){
+      board.togglePiece(row, i);
+
+      if(!board.hasAnyQueensConflicts()){
+        recurse(row+1);
+      }
+      board.togglePiece(row, i);
+    }
+  };
+
+  recurse(0);*/
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
