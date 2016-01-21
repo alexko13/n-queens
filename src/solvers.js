77163777e63cd2849b50a7395285574df600e3b0
Alex Ko <alexko@utexas.edu>
2016-01-21 16:23:48 -0700

    implemented findNQueensSolution

--- src/solvers.js
+++ src/solvers.js
@@ -65,43 +65,39 @@ window.countNRooksSolutions = function(n) {
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
   var solution = undefined; //fixme
-  var solutionCount = 0;
   var board = new Board({'n': n});
-  var temp = undefined;
+  var temp;
+  var foundSolution = false;
 
-  // for (var i = 0; i < n; i++){
-  //   for (var j = 0; j < n; j++){
-  //     board.togglePiece(i, j);
-  //     if(board.hasAnyQueensConflicts()){
-  //       board.togglePiece(i, j);
-  //     }
-  //   }
-  // }
+
 
   var recurse = function(row){
-    if (solution === undefined){
-      if (row === n){
-        solutionCount++;
-        console.log(solution);
-        solution = board.rows().slice();
-        console.log(solution);
-        //temp = JSON.stringify(board.rows());
-        console.log('--------------------------------------------');
-        console.log(JSON.stringify(board.rows()));
-        return;
+    if (row === n){
+      if(!foundSolution) {  
+        foundSolution = true;
+        return board.rows();
       }
-      for (var i = 0; i < n; i++){
-        board.togglePiece(row, i);
+      temp = JSON.stringify(board.rows());
+      console.log(temp);
+      return;
+    }
+    for (var i = 0; i < n; i++){
+      board.togglePiece(row, i);
 
-        if(!board.hasAnyQueensConflicts()){
-          recurse(row+1);
+      if(!board.hasAnyQueensConflicts()){
+        var x = recurse(row+1); 
+        if(x) {
+          return x;
         }
-        board.togglePiece(row, i);
+      }
+      if(!foundSolution){
+        board.togglePiece(row, i);  
       }
     }
   };
-  //solution = JSON.parse(temp);
-  recurse(0);
+
+  solution = recurse(0) || (new Board({'n':n})).rows();
+ 
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
 };
